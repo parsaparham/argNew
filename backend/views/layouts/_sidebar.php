b892cb91e711c18c9a384c4ebf0679061262d353
parsaparham <jafari2002@gmail.com>
2023-02-19 23:12:13 +0330

    At the Baharestan 30/11/1401 Time 11:11

--- backend/views/layouts/_sidebar.php
+++ backend/views/layouts/_sidebar.php
@@ -14,6 +14,34 @@
             [
                 'label' => 'Brands',
                 'url' => ['/brands/index']
+            ],
+            [
+                'label' => 'Categories',
+                'url' => ['/categories/index']
+            ],
+            [
+                'label' => 'Banks',
+                'url' => ['/banks/index']
+            ],
+            [
+                'label' => 'Order Status',
+                'url' => ['/orderstatus/index']
+            ],
+            [
+                'label' => 'Permissions',
+                'url' => ['/permissions/index']
+            ],
+            [
+                    'label' => 'Slide Shows',
+                'url' => ['/slideshows/index']
+            ],
+            [
+                    'label' => 'Products',
+                'url' => ['/products/index']
+            ],
+            [
+                    'label' => 'Videos',
+                'url' => ['video/index']
             ]
         ]
     ]) ?>
--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -21,6 +21,8 @@ AppAsset::register($this);
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <?php $this->registerCsrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+                rel="stylesheet">
         <?php $this->head() ?>
     </head>
     <body class="d-flex flex-column h-100">
